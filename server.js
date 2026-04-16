354599044c273d4574e67c93e3dca5794afbb83a
Sreejani Ganguly <sreejaniganguly07@gmail.com>
2026-04-16 20:27:39 +0530

    fixed render port issue

--- server.js
+++ server.js
@@ -87,6 +87,9 @@ Input: ${JSON.stringify(data)}`
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+
 });
